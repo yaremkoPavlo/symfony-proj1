f7d8c38241dbc293cd100890c2af600f045c9239
yaremkoPavlo <yaremko.pavlo@gmail.com>
2019-06-17 18:22:12 +0300

    getProducts methods realised

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,26 +3,32 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Product;
+
 #use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
     /**
-     * TODO:
      * Show only available product
      */
     public function getProducts()
     {
-        return $this->json(['a' => 'products']);
+        $repository = $this->getDoctrine()->getRepository(Product::class);
+        $products   = $repository->findAllAvailable();
+
+        return $this->json($this->getResponseHelper($products));
     }
 
     /**
-     * TODO:
      * Show all products
      */
     public function getAllProducts()
     {
-        return $this->json(['a' => 'All products']);
+        $repository = $this->getDoctrine()->getRepository(Product::class);
+        $products   = $repository->findAll();
+        
+        return $this->json($this->getResponseHelper($products));
     }
 
     /**
@@ -55,5 +61,21 @@ class ProductController extends AbstractController
     {
         print_r($request->request->all());
         return $this->getProduct($product_id);
+    }
+
+    /**
+     * @param array[Product]
+     * @return array
+     */
+    protected function getResponseHelper($products) {
+        $response = [];
+        foreach ($products as $product) {
+            $response[] = [
+                'name'      => $product->getName(),
+                'price'     => $product->getPrice(),
+                'quantity'  => $product->getQuantity()
+            ];
+        }
+        return $response;
     }
 }
